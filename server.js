e3064cbb1c6a12bada75afe2609839ad353a978f
lagron1000 <lagron1000@gmail.com>
2018-06-17 18:49:42 +0300

    heroku

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var app = express();
 app.use(express.static('node_modules'));
 app.use(express.static('public'));
 
-app.listen(8000, console.log('listening on port 8000'));
+app.listen(process.env.PORT || 8000, console.log('listening on port 8000'));
